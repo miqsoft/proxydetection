6971cd2957997fbfaa844653060195b129ac673a
miqsoft <mksaxhandball@gmail.com>
2025-04-01 09:22:33 +0200

    add airvpn

---
+++ experiments/airvpn/Readme.md
@@ -0,0 +1,11 @@
+# Infos:
+- use default ports of AirVPN only so OpenVPN: 443 (tcp and udp) WireGuard: 1637
+- configs due to keys and certificates are not included (create within AirVPN account and place them in the config folder)
+
+# Todo:
+- test with openvpn, wireguard client
+- test Hummingbird for openvpn
+
+# Version:
+- eddie-cli: 2.24.6
+- hummingbird: 1.3.0
---
+++ run.sh
@@ -0,0 +1,8 @@
+python3 experiment.py ./experiments/airvpn/eddie_openvpn_tcp_443/
+python3 experiment.py ./experiments/airvpn/eddie_openvpn_udp_443/
+python3 experiment.py ./experiments/airvpn/eddie_wireguard_1637/
+python3 experiment.py ./experiments/airvpn/hummingbird_openvpn_tcp_443/
+python3 experiment.py ./experiments/airvpn/hummingbird_openvpn_udp_443/
+python3 experiment.py ./experiments/airvpn/openvpn_tcp_443/
+python3 experiment.py ./experiments/airvpn/openvpn_udp_443/
+python3 experiment.py ./experiments/airvpn/wireguard_wireguard_1637/
